dd2bdb7754b2f3287946c2d5da6fd525855488c2
bradypeacock <38331774+bradypeacock@users.noreply.github.com>
2020-04-14 20:00:17 -0400

    Update api_mux_test.py

--- api_mux_test.py
+++ api_mux_test.py
@@ -8,6 +8,6 @@ device = API.Arduino('/dev/ttyACM0',9600)
 print("MUX TEST")
 pin=input("Please enter DUT pin you would like to set:\n  >> ")
 voltage=input("Please enter voltage you would like to set on pin "+pin+":\n  >> ")
-# skip 13, 14, 16, 17, 19, 26, 27, 28, 33, 42, 45
+
 device.select_pin(0, int(pin))
 device.force_voltage(0, float(voltage))
